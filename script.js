9e80c7d45c2f45d91aed318ee4851728757c765c
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-06-08 13:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateOutputText() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Hello, world!");
+      }, 1000);
+    });
+  }
+  
+  updateOutputText().then((message) => {
+    let outputElement = document.getElementById('output');
+    outputElement.innerText = message;
+  });
+  
